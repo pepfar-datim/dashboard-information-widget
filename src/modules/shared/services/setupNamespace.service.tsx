b75753793d229d5fbc859d9f92c8d8e55cfbfaf3
Peter Linnegan <plinnegan@baosystems.com>
2020-12-15 09:51:24 +0000

    fix: configuration key not being added when installed on servers where namespace already exists

--- src/modules/shared/services/setupNamespace.service.tsx
+++ src/modules/shared/services/setupNamespace.service.tsx
@@ -3,17 +3,27 @@ import { getKeyUid, shareKey } from './content.service';
 
 const config = require('../../../config/config.json');
 
+async function addNamespaceConfig(datastoreNamespace, onlyOpenToSuperUsersKey) {
+    const data = { [onlyOpenToSuperUsersKey]: false };
+    await api.post(`/dataStore/${datastoreNamespace}/configuration`, data);
+    //Share configuration key so only superusers can edit
+    const configKeyUid = await getKeyUid('configuration');
+    await shareKey(configKeyUid, 'r-------');
+}
+
 export default async function setupNamespace() {
-    // Check if namespace exists
+    const { datastoreNamespace, onlyOpenToSuperUsersKey } = config;
     const namespaces = await api.get('/dataStore');
     if (!namespaces.includes(config.datastoreNamespace)) {
         // Namespace does not yet exist
-        const { datastoreNamespace, onlyOpenToSuperUsersKey } = config;
         console.log(`Setting up namespace ${datastoreNamespace}`);
-        const data = { [onlyOpenToSuperUsersKey]: false };
-        await api.post(`/dataStore/${datastoreNamespace}/configuration`, data);
-        //Share configuration key so only superusers can edit
-        const configKeyUid = await getKeyUid('configuration');
-        await shareKey(configKeyUid, 'r-------');
+        await addNamespaceConfig(datastoreNamespace, onlyOpenToSuperUsersKey);
+    } else {
+        const namespaceKeys = await api.get(`/dataStore/${config.datastoreNamespace}`);
+        if (!('configuration' in namespaceKeys)) {
+            // Namespace exists, but no config key (upgrading from old app version)
+            console.log(`Adding configuration key to ${datastoreNamespace} namespace`);
+            await addNamespaceConfig(datastoreNamespace, onlyOpenToSuperUsersKey);
+        }
     }
 }
