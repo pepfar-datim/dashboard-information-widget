3da6eb8c7ba997bf6b8f0500da7a8d75754d372d
Jakub <jflaska@baosystems.com>
2020-12-21 12:17:36 -0500

    fix of filtering issue

--- src/modules/shared/services/contentUrl.service.tsx
+++ src/modules/shared/services/contentUrl.service.tsx
@@ -1,7 +1,8 @@
 const config = require('../../../config/config.json');
 
-export function getWidgetId() {
-    return window.location.search.replace('?dashboardItemId=', '');
+export function getWidgetId():string|null{
+    let re = window.location.search.match(/(\?|&)dashboardItemId\=([^&]*)/);
+    return re&&re[2];
 }
 
 export default function getContentUrl() {
