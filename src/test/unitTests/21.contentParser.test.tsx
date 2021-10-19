e9e6d0c7b4e69c036ffb14223af1d8aa1e1b0287
Jakub Flaska <jflaska@baosystems.com>
2021-10-19 14:32:23 -0400

    working

--- src/test/unitTests/21.contentParser.test.tsx
+++ src/test/unitTests/21.contentParser.test.tsx
@@ -7,7 +7,7 @@ import {
 
 type TestCase = {
     name: string;
-    dataStore:string;
+    dataStore:string|null;
     output: ContentItem[]
 }
 
@@ -39,11 +39,19 @@ const testCases:TestCase[] = [{
     name: 't<s>t<s>t',
     dataStore: "intro<pre type='swifter'>swifter1</pre>midsection<pre type='swifter'>swifter2</pre>end",
     output: [txt('intro'),sft("<pre type='swifter'>swifter1</pre>"),txt('midsection'),sft("<pre type='swifter'>swifter2</pre>"),txt('end')]
+},{
+    name: 'error scenario',
+    dataStore: '',
+    output: []
+},{
+    name: 'error scenario',
+    dataStore: null,
+    output: []
 }]
 
 testCases.forEach(({name, dataStore, output})=> {
     test(`Extract swifter code | ${name}`, () => {
-        let parsed:ContentItem[] = parseContent(dataStore);
+        let parsed:ContentItem[] = parseContent(dataStore as string);
         parsed.forEach((item:ContentItem,i:number)=>{
             if (typeof item ==='string') expect(item).toBe(output[i]);
         });
