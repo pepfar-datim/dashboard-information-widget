7887bf29b421ebb484190971929bc02af35319fa
Peter Linnegan <plinnegan@baosystems.com>
2022-03-04 17:26:46 +0000

    revert react-router-dom to older version

--- src/modules/main/components/routerWrapper.component.tsx
+++ src/modules/main/components/routerWrapper.component.tsx
@@ -1,22 +1,22 @@
-import React, {Ref} from 'react';
-import {HashRouter, Route, Routes} from 'react-router-dom';
+import React, { Ref } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Render from '../../render/components/render.component';
 import Edit from '../../edit/edit.component';
-import {SnackbarProvider, SnackbarProvider as MuiSnackbarProvider} from "notistack";
-import {IconButton} from "@mui/material";
-import {CheckCircle, Close} from "@mui/icons-material";
+import { SnackbarProvider, SnackbarProvider as MuiSnackbarProvider } from 'notistack';
+import { IconButton } from '@mui/material';
+import { CheckCircle, Close } from '@mui/icons-material';
 
 const notistackRef = React.createRef();
-const onClickDismiss = key => () => {
+const onClickDismiss = (key) => () => {
     // @ts-ignore
     notistackRef.current.closeSnackbar(key);
-}
+};
 
 const styles = {
     icon: {
         width: 20,
-        marginInlineEnd: 8
-    }
+        marginInlineEnd: 8,
+    },
 };
 
 export default function RouterWrapper() {
@@ -36,15 +36,16 @@ export default function RouterWrapper() {
                     </IconButton>
                 )}
                 iconVariant={{
-                    warning: <CheckCircle style={styles.icon}/>,
+                    warning: <CheckCircle style={styles.icon} />,
                 }}
             >
-                <Routes>
-                    <Route path="/" element={<Render/>}/>
-                    <Route path="/textEdit" element={<Edit/>}/>
-                </Routes>
+                <Route path={['/', '/edit']} exact>
+                    <Render />
+                </Route>
+                <Route path="/textEdit" exact>
+                    <Edit />
+                </Route>
             </SnackbarProvider>
         </HashRouter>
     );
 }
-
