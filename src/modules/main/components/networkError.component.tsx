1d91849ed8016550ba756272ae5a1b23af10a805
Jakub Flaska <jflaska@baosystems.com>
2022-09-22 12:18:43 -0400

    adding icon

--- src/modules/main/components/networkError.component.tsx
+++ src/modules/main/components/networkError.component.tsx
@@ -8,20 +8,24 @@ const styles = {
     }
 };
 
+function isDatim():boolean{
+    return window.location.href.includes('datim');
+}
+
 export default function NetworkError(){
     return(
         <Paper style={styles.root}>
             <Typography variant="h5">Connection Error</Typography>
             <Divider/>
-            <Typography>Cannot connect to DATIM</Typography>
+            <Typography>Cannot connect to DHIS2</Typography>
             <br/>
             <Typography>Please check the following:</Typography>
             <ul>
                 <li><Typography>You are still connected to the Internet</Typography></li>
-                <li><Typography>You are still logged in into DATIM</Typography></li>
+                <li><Typography>You are still logged in into DHIS2</Typography></li>
                 <li><Typography>You have the rights to use Dashboard Information Widget</Typography></li>
             </ul>
-            <Typography>If this problem persists, please contact <Link href='https://datim.zendesk.com/'>DATIM Support</Link></Typography>
+            {isDatim()&&<Typography>If this problem persists, please contact <Link href='https://datim.zendesk.com/'>DATIM Support</Link></Typography>}
         </Paper>
     );
 }
