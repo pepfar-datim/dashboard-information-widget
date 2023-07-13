cd5580c77a8392ac4c3750c6d8321e14a99af281
Maereg Sorri <msorri@baosystems.com>
2023-07-13 11:45:39 -0400

    [DAT-2254] fix for landing page wip

--- src/modules/shared/services/sanitize.service.tsx
+++ src/modules/shared/services/sanitize.service.tsx
@@ -2,5 +2,5 @@ import * as DOMPurify from 'dompurify';
 
 export default function sanitize(content:string):string{
     if (!content) return '';
-    return DOMPurify.sanitize(content, {ALLOWED_TAGS: ['iframe'],ADD_ATTR: ['target']});
+    return DOMPurify.sanitize(content, {ADD_TAGS: ["iframe"],ADD_ATTR: ['target']});
 }
