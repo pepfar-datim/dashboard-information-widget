03da75d93f82091e8ee3eebc8b4b865d528b8116
Maereg Sorri <msorri@baosystems.com>
2023-09-26 10:15:08 -0400

    [DAT-3177] adds on test

--- src/modules/main/services/setupNamespace.service.tsx
+++ src/modules/main/services/setupNamespace.service.tsx
@@ -1,4 +1,4 @@
-import {getJson, postJson } from "@pepfar-react-lib/datim-api";
+import {getJson, postJson, postEmpty } from "@pepfar-react-lib/datim-api";
 import {getKeyUid, shareKey} from "../../shared/services/shareKey.service";
 
 const config = require('../../../config/config.json');
@@ -6,6 +6,7 @@ const config = require('../../../config/config.json');
 async function addNamespaceConfig(datastoreNamespace, onlyOpenToSuperUsersKey) {
     const data = { [onlyOpenToSuperUsersKey]: false };
     await postJson(`/dataStore/${datastoreNamespace}/configuration`, data);
+    await postEmpty('/maintenance/cache');
     const configKeyUid = await getKeyUid('configuration');
     await shareKey(configKeyUid, 'r-------');
 }
@@ -21,5 +22,5 @@ async function checkDataStore(){
 export async function connectToDataStore(){
     if(await checkDataStore()) return;
     const {datastoreNamespace, onlyOpenToSuperUsersKey} = config;
-    await addNamespaceConfig(datastoreNamespace, onlyOpenToSuperUsersKey);
+    await addNamespaceConfig(datastoreNamespace, onlyOpenToSuperUsersKey);   
 }
