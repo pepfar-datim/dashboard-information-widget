51e81696f8e148cc494b95b7e47502acf66394f6
Pete Linnegan <plinnegan@Petes-MacBook-Pro.local>
2024-07-30 16:47:23 +0100

    fix: handle setting up allowed urls datastore if missing

--- render/src/main/fetchAllowedUrls.service.ts
+++ render/src/main/fetchAllowedUrls.service.ts
@@ -3,25 +3,36 @@ const defaultAllowedUrls = [
   'https://www.youtube.com/',
 ]
 
+async function setupAllowedUrls() {
+  const req = await fetch('/api/dataStore/dashboard-information/')
+  const res = await req.json()
+  if (!res.includes('allowedUrls')) {
+    const setupReq = await fetch(
+      '/api/dataStore/dashboard-information/allowedUrls',
+      {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(defaultAllowedUrls),
+      }
+    ) 
+    const setupRes = await setupReq.json()
+    console.log('Allowed URLs datastore setup: ', setupRes)
+  }
+}
+
 export default async function fetchAllowedUrls(): Promise<string[]> {
   try {
     const req = await fetch('/api/dataStore/dashboard-information/allowedUrls')
-    return await req.json()
-  } catch (e) {
-    const req = await fetch('/api/dataStore/dashboard-information/')
     const res = await req.json()
-    if (!res.includes('allowedUrls')) {
-      const setupReq = await fetch(
-        '/api/dataStore/dashboard-information/allowedUrls',
-        {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify(defaultAllowedUrls),
-        }
-      )
-      const setupRes = await setupReq.json()
-      console.log('Allowed URLs datastore setup: ', setupRes)
+    console.log('Allowed URLs fetched: ', res)
+    if (res?.status === 'SUCCESS') {
+      return await res
+    } else {
+      setupAllowedUrls()
+      return defaultAllowedUrls
     }
+  } catch (e) {
+    setupAllowedUrls()
     return defaultAllowedUrls
   }
 }
